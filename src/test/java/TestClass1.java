f55b2e68fdf40328c89a397421155ae88ddc240b
nagqatt <nagqatt@yahoo.co.uk>
2021-02-14 21:06:44 +0000

    Jira - 546545 - New Test added

--- src/test/java/TestClass1.java
+++ src/test/java/TestClass1.java
@@ -10,5 +10,9 @@ public class TestClass1 {
     public void test2(){
         System.out.println("Running test 2");
     }
+    @Test
+    public void test3(){
+        System.out.println("Running added test 3");
+    }
 
 }
