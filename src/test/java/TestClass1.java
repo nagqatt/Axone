a28c9a8d4fb586db55cfb593830144de4ae19ff7
nagqatt <nagqatt@yahoo.co.uk>
2021-02-14 20:23:09 +0000

    Added another test in the unit test

--- src/test/java/TestClass1.java
+++ src/test/java/TestClass1.java
@@ -4,9 +4,11 @@ public class TestClass1 {
 
     @Test
     public void test1(){
-        System.out.println("Test Annotation on Manven from Junit");
+        System.out.println("Running test 1");
     }
-
-
+    @Test
+    public void test2(){
+        System.out.println("Running test 2");
+    }
 
 }
